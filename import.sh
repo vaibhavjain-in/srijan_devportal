05819722f838d54f6a5254035150ac9129561dbe
sja112 <jainsandesh94@gmail.com>
2019-05-20 10:52:58 +0530

    Updated import.sh file to download libraries.

--- import.sh
+++ import.sh
@@ -9,7 +9,9 @@ cd ../../..
 # Create new /libraries folder inside /web folder.
 mkdir -p libraries
 echo "Running coniguration import (slider libraries download)"
-../vendor/bin/drush dl-cycle-lib
 
-# Clear Cache.
-../vendor/bin/drush cr
+# Change directory form libraries folder, create new folder and inside that folder download file.
+cd libraries && mkdir -p jquery.cycle && cd jquery.cycle && curl -O https://raw.githubusercontent.com/malsup/cycle/3.0.3/jquery.cycle.all.js
+cd ../../libraries && mkdir -p json2 && cd json2 && curl -O https://raw.githubusercontent.com/douglascrockford/JSON-js/master/json2.js
+cd ../../libraries && mkdir -p jquery.hoverIntent && cd jquery.hoverIntent && curl -O https://raw.githubusercontent.com/briancherne/jquery-hoverIntent/master/jquery.hoverIntent.js
+cd ../../libraries && mkdir -p jquery.pause && cd jquery.pause && curl -O https://raw.githubusercontent.com/tobia/Pause/master/jquery.pause.js
